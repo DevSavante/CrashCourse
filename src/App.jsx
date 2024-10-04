d8d780c6891131e5c6f39b545c1d3025b0cf4542
Lexine <koteilexine91@gmail.com>
2024-10-04 17:01:42 +0000

    Additional commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,16 @@
+import {Route, createBrowserRouter, createRoutesFromElements, RouterProvider,  } from 'react-router-dom';
 import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
 import HomeCards from "./components/HomeCards"
-<<<<<<< HEAD
+import JobListings from "./components/JobListings" // Keep this from the remote
+import ViewAllJobs from "./components/ViewAllJobs"
 
-=======
-import JobListings from "./components/JobListings"
->>>>>>> 352f5047a45b00d64dba549c4c035fa25a68389d
+const router = createBrowserRouter(
+  createRoutesFromElements(<Route path="/" element={<h1>My App</h1>}/>)
+);
+
+
 const App = () => {
-  return (
-
-    <>
-    <Navbar />
-    <Hero />
-   <HomeCards />
-   <JobListings />
-    
-    
-    
-
-<<<<<<< HEAD
-   {/* <!-- Browse Jobs --> */}
-   
-
-=======
-  
-    
->>>>>>> 352f5047a45b00d64dba549c4c035fa25a68389d
-    <section className="m-auto max-w-lg my-10 px-6">
-      <a
-        href="jobs.html"
-        className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
-        >View All Jobs</a
-      >
-    </section>
-
-
-   
-    </>
-  )
-}
-
+  return <RouterProvider router={router} />
+  };
 export default App
