312f1f55048d954baf381a4a7cf4a8ace14b904f
Lexine <koteilexine91@gmail.com>
2024-10-07 17:02:19 +0000

    Recent commits

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import {Route, createBrowserRouter, createRoutesFromElements, RouterProvider,  } from 'react-router-dom';
-import Navbar from "./components/Navbar"
-import Hero from "./components/Hero"
-import HomeCards from "./components/HomeCards"
-import JobListings from "./components/JobListings" // Keep this from the remote
-import ViewAllJobs from "./components/ViewAllJobs"
+import MainLayout from './layouts/MainLayout';
+import HomePage from './pages/HomePage';
+import JobsPage from './pages/JobsPage';
+import NotFoundPage from './pages/NotFoundPage';
 
 const router = createBrowserRouter(
-  createRoutesFromElements(<Route path="/" element={<h1>My App</h1>}/>)
+  createRoutesFromElements(
+  <Route path='/' element={<MainLayout />}>
+  <Route index element={<HomePage />} />
+  <Route path='/jobs' element ={<JobsPage />}/>
+  <Route path='*' element ={<NotFoundPage />}/>
+  </Route>
+  )
 );
 
 
